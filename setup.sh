99157ca3f19706e35125a1bfa84aaafba28b1dcf
Phillip Boles <phillip.boles@armor.com>
2025-04-14 22:44:51 -0500

    refactor suite

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+pip install -r tools/api-fuzzer/requirements.txt
+pip install -r tools/header-injector/word_fuzzer/requirements.txt
+pip install -r tools/jwt-manipulator/jwt_inspection/requirements.txt
+pip install -r tools/pyapi-validator/requirements.txt
+pip install -r tools/spec-analyzer/requirements.txt
