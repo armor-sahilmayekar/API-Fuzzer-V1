44965eda8bee48013086cdd9d6ad1511fd82a107
Phillip Boles <phillip.boles@armor.com>
2025-04-29 20:57:23 -0500

    fixing environment setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-pip3 install -r tools/api-fuzzer/requirements.txt
-pip3 install -r tools/header-injector/word_fuzzer/requirements.txt
-pip3 install -r tools/jwt-manipulator/jwt_inspection/requirements.txt
-pip3 install -r tools/pyapi-validator/requirements.txt
-pip3 install -r tools/spec-analyzer/requirements.txt
+pip3 install --upgrade pip
+pip3 install --no-cache-dir  -r requirements.txt
+pip3 install --no-cache-dir -r tools/api-fuzzer/requirements.txt
+pip3 install --no-cache-dir -r tools/header-injector/word_fuzzer/requirements.txt
+pip3 install --no-cache-dir -r tools/jwt-manipulator/jwt_inspection/requirements.txt
+pip3 install --no-cache-dir -r tools/pyapi-validator/requirements.txt
+pip3 install --no-cache-dir -r tools/spec-analyzer/requirements.txt
