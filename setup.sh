b54f9f3efd7d28b598f3985df7d6c3df56723a1c
Phillip Boles <phillip.boles@armor.com>
2025-04-29 21:07:38 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -e
+
+VENV_DIR=".venv"
+
+# Create virtual environment if not exists
+if [ ! -d "$VENV_DIR" ]; then
+  python3 -m venv "$VENV_DIR"
+fi
+
+# Activate virtual environment
+source "$VENV_DIR/bin/activate"
 
 pip3 install --upgrade pip
 pip3 install --no-cache-dir  -r requirements.txt
